f45312b273cca5e63c52cd3504a9b1971212012c
VladislavLinevich <ulad.linevich@outlook.com>
2022-05-19 19:58:01 +0300

    Added queryset filter

--- movie/urls.py
+++ movie/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MovieListView.as_view(), name='movies'),
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
     path('actor/<int:pk>/', views.ActorDetailView.as_view(), name='actor-detail'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -2,8 +2,9 @@ from multiprocessing import context
 from pyexpat import model
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import Category, Movie, Actor
+from .models import Category, Movie, Actor, Genre
 from django.views import generic
+from django.db.models import Q
 
 class MovieListView(generic.ListView):
     """Список фильмов"""
@@ -12,8 +13,10 @@ class MovieListView(generic.ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["movies"] = Movie.objects.filter(draft=False).order_by("id")[:4]
+        context["last_movies"] = Movie.objects.filter(draft=False).order_by("-id")[:4]
         context["categories"] = Category.objects.all()
+        context["genres"] = Genre.objects.all()
+        context["years"] = Movie.objects.filter(draft=False).values_list("year", flat=True).distinct("year")
         return context
 
 class MovieDetailView(generic.DetailView):
@@ -22,8 +25,10 @@ class MovieDetailView(generic.DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["movies"] = Movie.objects.filter(draft=False).order_by("id")[:4]
+        context["last_movies"] = Movie.objects.filter(draft=False).order_by("-id")[:4]
         context["categories"] = Category.objects.all()
+        context["genres"] = Genre.objects.all()
+        context["years"] = Movie.objects.filter(draft=False).values_list("year", flat=True).distinct("year")
         return context
 
 class ActorDetailView(generic.DetailView):
@@ -32,6 +37,48 @@ class ActorDetailView(generic.DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["movies"] = Movie.objects.filter(draft=False).order_by("id")[:4]
+        context["last_movies"] = Movie.objects.filter(draft=False).order_by("-id")[:4]
         context["categories"] = Category.objects.all()
+        context["genres"] = Genre.objects.all()
+        context["years"] = Movie.objects.filter(draft=False).values_list("year", flat=True).distinct("year")
         return context
+
+class FilterMoviesView(generic.ListView):
+    """Фильтр фильмов"""
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["last_movies"] = Movie.objects.filter(draft=False).order_by("-id")[:4]
+        context["categories"] = Category.objects.all()
+        context["genres"] = Genre.objects.all()
+        context["years"] = Movie.objects.filter(draft=False).values_list("year", flat=True).distinct("year")
+        return context
+    
+    def get_queryset(self):
+        if self.request.GET.getlist("genre") and self.request.GET.getlist("year") and self.request.GET.getlist("category"):
+            queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist("year")) &
+                Q(genres__in=self.request.GET.getlist("genre")) &
+                Q(category__in=self.request.GET.getlist("category"))
+            )
+        elif self.request.GET.getlist("genre") and self.request.GET.getlist("category"):
+            queryset = Movie.objects.filter(
+                Q(genres__in=self.request.GET.getlist("genre")) &
+                Q(category__in=self.request.GET.getlist("category"))
+            )
+        elif self.request.GET.getlist("category") and self.request.GET.getlist("year"):
+            queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist("year")) &
+                Q(category__in=self.request.GET.getlist("category"))
+            )
+        elif self.request.GET.getlist("genre") and self.request.GET.getlist("year"):
+            queryset = Movie.objects.filter(
+                Q(genres__in=self.request.GET.getlist("genre")) &
+                Q(year__in=self.request.GET.getlist("year"))
+            )
+        else:
+            queryset = Movie.objects.filter(
+                Q(year__in=self.request.GET.getlist("year")) |
+                Q(genres__in=self.request.GET.getlist("genre")) |
+                Q(category__in=self.request.GET.getlist("category"))
+            )
+        return queryset
